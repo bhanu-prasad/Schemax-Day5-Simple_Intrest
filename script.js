ea3b2751e7396ffda69f0cd2ae1e9f0f0ed75b6e
bhanu-prasad <bhanu.prasad1908@gmail.com>
2021-05-04 13:23:27 +0530

    1

---
+++ script.js
@@ -0,0 +1,32 @@
+function isNumberKey(evt)
+    {
+      var charCode = (evt.which) ? evt.which : event.keyCode;
+      if ((charCode < 48 || charCode > 57) && charCode!==46)
+      return false;
+    
+      return true;
+    }
+    function isNumberKeytime(evt)
+    {
+      var charCode = (evt.which) ? evt.which : event.keyCode;
+      if (charCode < 48 || charCode > 57)
+      return false;
+    
+      return true;
+    }
+
+    function calculate(){
+        var p = Number(document.getElementById("principle").value);
+        var t = Number(document.getElementById("time").value);
+        var r = Number(document.getElementById("rate").value);
+        
+        
+        var intrest = p*t*r/100;
+
+        var final = intrest+p;
+        console.log(typeof intrest);
+        document.getElementById("intrest").innerHTML = "₨. "+intrest;
+        document.getElementById("final").innerHTML = "₨. "+final;
+         
+        
+    }
